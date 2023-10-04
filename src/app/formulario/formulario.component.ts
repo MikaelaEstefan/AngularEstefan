d8a61b77697dae23f512cfc15f759d73b03677ac
Mikaela Estefan <estefanmikaela@gmail.com>
2023-10-04 00:22:42 -0300

    formulario update

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -8,15 +8,17 @@ import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/fo
 })
 export class FormularioComponent {
   formulario: FormGroup;
+  asignaturasValidas: string[] = ['Matemáticas', 'Ciencias', 'Historia', 'Literatura'];
 
   constructor(private fb: FormBuilder) {
     this.formulario = this.fb.group({
-      nombre: ['', [Validators.required, this.validarNombre.bind(this)]], // Usa .bind(this) para mantener el contexto
-      email: ['', [Validators.required, Validators.email]]
+      nombre: ['', [Validators.required, this.validarNombre.bind(this)]], 
+      email: ['', [Validators.required, Validators.email]],
+      asignatura: ['', [Validators.required, this.validarAsignatura.bind(this)]]
     });
   }
 
-  validarNombre(control: AbstractControl) { // Especifica el tipo de control
+  validarNombre(control: AbstractControl) {
     const nombre = control.value;
     if (nombre === 'admin' || nombre === 'root') {
       return { nombreInvalido: true };
@@ -24,7 +26,16 @@ export class FormularioComponent {
     return null;
   }
 
-  // Función para obtener los errores de validación
+  validarAsignatura(control: AbstractControl) {
+    const asignatura = control.value;
+    if (asignatura === '') {
+      return { asignaturaObligatoria: true };
+    } else if (!this.asignaturasValidas.includes(asignatura)) {
+      return { asignaturaInvalida: true, opcionesValidas: this.asignaturasValidas.join(', ') };
+    }
+    return null;
+  }
+  
   getErrors(controlName: string) {
     const control = this.formulario.get(controlName);
     return control?.errors ? Object.keys(control.errors) : [];
@@ -32,7 +43,7 @@ export class FormularioComponent {
 
   onSubmit() {
     if (this.formulario.valid) {
-      // Procesa el formulario aquí
+      
       console.log(this.formulario.value);
     }
   }
